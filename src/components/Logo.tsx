e9088f3c353809a3f361cee55c09e348b2f3eb92
arif matubber <am@hoxro.com>
2025-08-14 00:35:48 +0600

    s

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 export function Logo() {
   return <div className="flex flex-col max-md:w-[100px]">
-    <img src="/public/Logo.webp" alt="" className=" w-[250px]" />
+    <img src="/Logo.webp" alt="" className=" w-[250px]" />
   </div>;
 }
